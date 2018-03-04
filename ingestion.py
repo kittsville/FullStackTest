c8fa125c24a237db2ab1ee6f8024ce7770fdb26e
Kit <kittsville@googlemail.com>
2018-03-04 17:22:38 +0000

    Add ingestion debugging

--- ingestion.py
+++ ingestion.py
@@ -14,6 +14,8 @@ c.execute("INSERT INTO recipes VALUES ('Bacon', 'bacon', 90)")
 # Save (commit) the changes
 conn.commit()
 
+print 'Saved 3 recipes to the database'
+
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 conn.close()
